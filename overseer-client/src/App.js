41b78da55047d8c7b88db6184dd069b530471592
Sebastian352 <sebyolaru44@yahoo.com>
2021-11-20 21:39:56 +0200

    Add monitor files

--- overseer-client/src/App.js
+++ overseer-client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { TestPS } from './components/input';
 
 function App() {
   return (
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and sve to reload.
         </p>
+        <TestPS></TestPS>
         <a
           className="App-link"
           href="https://reactjs.org"
