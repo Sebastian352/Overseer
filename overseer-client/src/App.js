c9e49f17047d304da3a40fd03da870020cc5b003
Stelian Stoian <stoianstelian33@gmail.com>
2021-11-21 12:17:24 +0200

    Finish New UI Integration

--- overseer-client/src/App.js
+++ overseer-client/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import { TestPS } from './components/testpowershell';
 import { AppList } from './components/appList';
 
 function App() {
   return (
     <div className="App">
-      <TestPS></TestPS>
       <AppList></AppList>
 
     </div>
